8bd504f990a5e9d4c2b7a2e5f5147b25c74c2b6f
DmitryKhilazhev <dkhilazhev@mail.ru>
2022-12-06 19:55:09 +0300

    Count App
    Sprint 3 final task h\w for Yandex Practicum

--- CounterApp/ViewController.swift
+++ CounterApp/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countButton: UIButton!
+    @IBOutlet weak var countLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    private var count:Int = 0
+    @IBAction func buttonDidTab(_ sender: Any) {
+        count += 1
+        countLabel.text = "Значение счетчика: \(count)"
+    }
 }
 
